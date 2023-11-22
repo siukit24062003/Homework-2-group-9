a5d752b495f3b6b392c528adebf7859334018e5f
siukit24062003 <luutuankiet2406@gmail.com>
2023-11-22 22:03:20 +0700

    hw2 microprocessor

--- Master/Master/main.asm
+++ Master/Master/main.asm
@@ -1,19 +1,19 @@
 .org 0
-rjmp main
+rjmp main			; Jump to the main routine
 
 .org $0002
-rjmp exint0
+rjmp exint0			; Jump to the exint0 routine
 
-main:
-sbi portb, 1
+main:	
+sbi portb, 1		; Set bit 1 of port B
 call init_port
 call init_lcd
 call spiinit
 call uartinit
 call init_int0
 
-loop:
-cbi portb, 1
+loop:	
+cbi portb, 1		; Clear bit 1 of port B
 rjmp loop
 
 init_port:
@@ -23,15 +23,15 @@ call delay_20ms
 ret
 
 init_lcd:
-ldi r16, 0x02
+ldi r16, 0x02		; Load 0x02 for LCD initialization
 call lcd_command
-ldi r16, 0x28
+ldi r16, 0x28		; Load 0x28 for LCD initialization
 call lcd_command
-ldi r16, 0x0e
+ldi r16, 0x0e		; Load 0x0e for LCD initialization
 call lcd_command
-ldi r16, 0x01
+ldi r16, 0x01		; Load 0x01 for LCD initialization
 call lcd_command
-ldi r16, 0x80
+ldi r16, 0x80		; Load 0x80 for LCD initialization
 call lcd_command
 ret
 
@@ -44,8 +44,8 @@ sbi portb, 4
 ret
 
 spiout:
-cbi portb, 4
-out spdr0, r17
+cbi portb, 4		; Clear PB4 
+out spdr0, r17		; Load data to SPI data register
 
 wait_transmit:
 in r16, spsr0
@@ -56,36 +56,36 @@ in r17, spdr0
 ret
 
 uartinit:
-ldi r16, 12
+ldi r16, 12			; load 12 for UART baud rate setting
 sts ubrr0l, r16
 ldi r16, (1 << u2x0)
 sts ucsr0a, r16
-ldi r16, (1 << rxen0) | (1 << txen0)
+ldi r16, (1 << rxen0) | (1 << txen0) ; load the value that is shift 1 bit to the left of receive Enable and rransmit Enable bits to r16 
 sts ucsr0b, r16
 ldi r16, (1 << ucsz01) | (1 << ucsz00)
 sts ucsr0c, r16
 ret
 
 uartout:
-lds r16, ucsr0a
+lds r16, ucsr0a		; load UART control and status register A
 sbrs r16, udre0
-rjmp uartout
-sts udr0, r17
+rjmp uartout		
+sts udr0, r17		; coad data to UART data register
 ret
 
 data_receive:
 lds r16, ucsr0a
 sbrs r16, rxc0
-rjmp data_receive
+rjmp data_receive	; continue waiting for data reception
 lds r17, udr0
 ret
 
 init_int0:
 sei
-ldi r16, (1 << isc01)
+ldi r16, (1 << isc01)	
 sts eicra, r16
 ldi r16, (1 << int0)
-out eimsk, r16
+out eimsk, r16		; set external interrupt mask register
 ret
 
 lcd_command:
@@ -98,19 +98,19 @@ rcall sdelay
 cbi porta, 2
 rcall delay_100us
 
-swap r16
+swap r16			; swap the nibbles of the register
 andi r16, 0xf0
 out porta, r16
 sbi porta, 2
 rcall sdelay
-cbi porta, 2
+cbi porta, 2		; clear PA2
 rcall delay_100us
 ret
 
 lcd_data:
 rcall delay_20ms
 mov r18, r16
-andi r18, 0xf0
+andi r18, 0xf0	
 out porta, r18
 sbi porta, 0
 sbi porta, 2
@@ -134,18 +134,18 @@ nop
 ret
 
 delay_100us:
-push r17
-ldi r17,100
+push r17			; save r17 on the stack
+ldi r17,100			; load 100 for the delay counter
 dl1:
 call sdelay
 dec r17
-brne dl1
+brne dl1			
 pop r17
 ret
 
 delay_2ms:
-push r17
-ldi r17,20
+push r17			; save r17 on the stack
+ldi r17,20			; load 20 for the delay counter
 ldr0:
 call delay_100us
 dec r17
@@ -159,7 +159,7 @@ ldi r17, 10
 powerup:
 call delay_2ms
 dec r17
-brne powerup
+brne powerup		;if r17 not equal to 0, jump to powerup
 pop r17
 ret
 
@@ -167,13 +167,13 @@ exint0:
 ldi r17, 0
 call spiout
 sbi portb, 1
-cpi r17, 0xff
+cpi r17, 0xff		; compare (CPI) instruction compares register r17 with the immediate value 0xFF.
 breq clear
-mov r16, r17
+mov r16, r17		; move r17 into r16.
 call lcd_data
 ldi r16, 0x80
 call lcd_command
-rjmp next
+rjmp next			; rejump to the next subroutine
 
 clear:
 ldi r16, 0x01
--- Slave/Slave/main.asm
+++ Slave/Slave/main.asm
@@ -1,31 +1,31 @@
 .org 0
 
 main:
-call portinit
+call portinit		; call portinit subroutine
 call spiinit
 
 loop:
-sbic pina, 0
-rjmp loop
+sbic pina, 0		; skip if bit 0 is pina is clear
+rjmp loop			; rejump to loop subroutine
 sbi porta, 1
 call scankey
 call spiin
 rjmp loop
 
 portinit:
-ldi r16, 2
+ldi r16, 2			; load 2 to r16
 out ddra, r16
-ldi r16, (1 << 6) | (1 << 1)
+ldi r16, (1 << 6) | (1 << 1)	; load binary value 01000010 into register r16
 out ddrb, r16
 ldi r16, 0x0f
-out ddrd, r16
-ldi r16, 0xf0
+out ddrd, r16		; set data direction register D to configure pins 7-4 as outputs
+ldi r16, 0xf0		; load 0xf0 to r16
 out portd, r16
 ret
 
 spiinit:
-ldi r16, (1 << spe0)
-out spcr0, r16
+ldi r16, (1 << spe0)	; load the value that shift left 1 bit of spe0 to r16
+out spcr0, r16			
 ret
 
 spiin:
@@ -34,38 +34,38 @@ cbi porta, 1
 sspi_wait_receive:
 in r16, spsr0
 sbrs r16, spif0
-rjmp sspi_wait_receive
+rjmp sspi_wait_receive	; rejump to sspi_wait_receive subroutine
 sbi porta, 1
 ret
 
 scankey:
 ldi r20, 0x0f
 out portd, r20
-ldi r22, 0b11110111
-ldi r23, 0
+ldi r22, 0b11110111		; load  247 to r22
+ldi r23, 0				; load 0 to r23
 ldi r24, 3
 
 keypad_scan_loop:
 out portd, r22
 call delay
-sbic pind, 4
-rjmp keypad_scan_check_col_2
+sbic pind, 4					; skip if bit 4 of pind is clear
+rjmp keypad_scan_check_col_2	; rejump to keypad_scan_check_col_2 subroutine
 rjmp keypad_scan_found
 
 keypad_scan_check_col_2:
-sbic pind, 5
+sbic pind, 5					; skip if bit 5 of pind is clear
 rjmp keypad_scan_check_col_3
 ldi r23, 1
 rjmp keypad_scan_found
 
 keypad_scan_check_col_3:
-sbic pind, 6
+sbic pind, 6					; skip if bit 6 of pind is clear
 rjmp keypad_scan_check_col_4
 ldi r23, 2
 rjmp keypad_scan_found
 
 keypad_scan_check_col_4:
-sbic pind, 7
+sbic pind, 7					; skip if bit 7 of pind is clear
 rjmp keypad_scan_next_row
 ldi r23, 3
 rjmp keypad_scan_found
@@ -73,15 +73,16 @@ rjmp keypad_scan_found
 keypad_scan_next_row:
 cpi r24, 0
 breq keypad_scan_not_found
-ror r22
+ror r22							; rotate right rhrough rarry on register r22
 dec r24
 rjmp keypad_scan_loop
 
 keypad_scan_found:
+; left shift the contents of register r23 twice to position the keypad value
 lsl r23
 lsl r23
 add r23, r24 ; keypad value in r23
-mov r17, r23
+mov r17, r23 ; move r23 to r17 
 cpi r17, 10
 brlo digit
 subi r17, -87
@@ -95,10 +96,10 @@ ldi r17, 0xff
 ret
 
 delay:
-ldi r16, 0b00001010
-sts tccr1b, r16
-ldi r16, 0x27
-sts ocr1ah, r16
+ldi r16, 0b00001010			 ; load the binary value 00001010 into register r16
+sts tccr1b, r16					
+ldi r16, 0x27				 ; load 0x27 to r16
+sts ocr1ah, r16					
 ldi r16, 0x10
 sts ocr1al, r16
 clr r16
@@ -106,7 +107,7 @@ sts tcnt1h, r16
 sts tcnt1l, r16
 
 delayloop:
-sbis tifr1, ocf1a
+sbis tifr1, ocf1a			; skip if bit 1 of timer/counter interrupt flag register 1 is set
 rjmp delayloop
 sbi tifr1, ocf1a
 clr r16
